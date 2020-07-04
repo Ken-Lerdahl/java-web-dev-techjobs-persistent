12da61b7165591e882f42b3c816c23f94f5b3e30
Ken-Lerdahl <ken.lerdahl@gmail.com>
2020-07-04 11:43:26 -0500

    Finished assignment. Set up one to one and many to many repository relationships, re-enabled list, view, and search functionality.

--- queries.sql
+++ queries.sql
@@ -1,9 +1,16 @@
 ## Part 1: Test it with SQL
 SHOW FIELDS FROM job;
+
 ## Part 2: Test it with SQL
 SELECT name
-FROM employer
+FROM employer;
 WHERE (location = "St. Louis City")
+
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT skill.name, skill.description
+FROM skill
+INNER JOIN job_skills ON (skill.id = job_skills.skills_id)
+WHERE job_skills.jobs_id IS NOT NULL;
